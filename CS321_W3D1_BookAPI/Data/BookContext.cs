6fcd9b34b5a4fc2564eed2c2d55dbe4c95080453
ericcoker18 <ericcoker18@Yahoo.com>
2021-04-12 19:01:52 -0500

    Services Updated

--- CS321_W3D1_BookAPI/Data/BookContext.cs
+++ CS321_W3D1_BookAPI/Data/BookContext.cs
@@ -5,6 +5,7 @@ namespace CS321_W3D1_BookAPI.Data
 {
     public class BookContext : DbContext
     {
+        public DbSet<Book> Books { get; set; }
         // TODO: implement a DbSet<Book> property
 
         public BookContext(DbContextOptions options) : base(options)
@@ -12,12 +13,29 @@ namespace CS321_W3D1_BookAPI.Data
             
         }
 
+        
+
         // This method runs once when the DbContext is first used.
         // It's a place where we can customize how EF Core maps our
         // model to the database. 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            // TODO: configure some seed data in the books table
+            modelBuilder.Entity<Book>().HasData(
+                new Book
+                {
+                    Id = 1,
+                    Title = "The Fellow Ship of the Ring",
+                    Author = "J.R.R Tolkien",
+                    Category = "Fantasy"
+                },
+                new Book
+                {
+                    Id = 1,
+                    Title = "The Fellow Ship of the Ring",
+                    Author = "J.R.R Tolkien",
+                    Category = "Fantasy"
+
+                });
             
             base.OnModelCreating(modelBuilder);
             
--- CS321_W3D1_BookAPI/Models/Book.cs
+++ CS321_W3D1_BookAPI/Models/Book.cs
@@ -5,6 +5,12 @@ namespace CS321_W3D1_BookAPI.Models
 {
     public class Book
     {
+ 
         public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Author { get; set; }
+        public string Category { get; set; }
     }
 }
